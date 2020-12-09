a3b34db19908f5e70356abaad9b31b56903a349a
encodeous <encodeous7@gmail.com>
2020-12-08 19:09:12 -0500

    Initial Commit

---
+++ Services/SearchService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using Microsoft.Extensions.Configuration;
+
+namespace emojipad.Services
+{
+    public class SearchService
+    {
+        private readonly IConfiguration _configuration;
+        private IndexWriter Index;
+
+        public SearchService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var indexPath = Path.Combine(Environment.CurrentDirectory, "index");
+
+            using var dir = FSDirectory.Open(indexPath);
+            var analyzer = new StandardAnalyzer(LuceneVersion.LUCENE_48);
+            var indexConfig = new IndexWriterConfig(LuceneVersion.LUCENE_48, analyzer);
+            Index = new IndexWriter(dir, indexConfig);
+        }
+    }
+}
---
+++ Shared/EmojiIndex.cs
@@ -0,0 +1,8 @@
+﻿namespace emojipad.Shared
+{
+    public class EmojiIndex
+    {
+        public string FileName { get; set; }
+        public int UsedFrequency { get; set; }
+    }
+}
