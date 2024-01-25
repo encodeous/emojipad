18a59e3af8aa19776f9f9fa9e957f087d7ca8ad3
Adam Chen <encodeous7@gmail.com>
2024-01-25 09:28:51 -0500

    Begin work on neo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,104 +1,36 @@
-﻿using EmojiPad.Data;
-using EmojiPad.Services;
-using EmojiPad.Utils;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
-namespace EmojiPad
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace emojipad
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public partial class MainWindow : Window
+    public sealed partial class MainWindow : Window
     {
-        public static MainWindow Instance;
         public MainWindow()
         {
-            var model = Utilities.GetService<EmojiPadModel>();
-            DataContext = model;
-            Instance = this;
-            InitializeComponent();
-            var cfg = Utilities.GetService<EmojiPadConfiguration>();
-            if (!cfg.ShowOnStart)
-            {
-                Hide();
-            }
-            Topmost = cfg.AlwaysOnTop;
-            SearchBox.Focus();
-            ServiceHost.Instance.AddEmojis();
+            this.InitializeComponent();
         }
 
-        private void OnSettingsClick(object sender, MouseButtonEventArgs e)
+        private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            var wnd = new Settings();
-            wnd.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            wnd.ShowDialog();
-        }
-
-        private void OnFolderClick(object sender, MouseButtonEventArgs e)
-        {
-            var cfg = Utilities.GetService<EmojiPadConfiguration>();
-            Process.Start(new ProcessStartInfo()
-            {
-                FileName = cfg.EmojiFolderPath + System.IO.Path.DirectorySeparatorChar,
-                UseShellExecute = true,
-                Verb = "open"
-            });
-        }
-        
-        private void OnRefreshClick(object sender, MouseButtonEventArgs e)
-        {
-            var cfg = Utilities.GetService<FileService>();
-            cfg.Sync();
-        }
-
-        private void Window_Closing(object sender, CancelEventArgs e)
-        {
-            var cfg = Utilities.GetService<EmojiPadConfiguration>();
-            if (cfg.CloseToTray)
-            {
-                e.Cancel = true;
-                Hide();
-            }
-        }
-
-        private void Window_Deactivated(object sender, EventArgs e)
-        {
-            var cfg = Utilities.GetService<EmojiPadConfiguration>();
-            if (cfg.HideAfterLostFocus)
-            {
-                Hide();
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Utilities.GetService<EmojiPadModel>().SearchQuery = "";
-        }
-
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (Mouse.LeftButton == MouseButtonState.Pressed)
-                this.DragMove();
+            myButton.Content = "Clicked";
         }
     }
 }
